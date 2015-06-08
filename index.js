85efc9d6c2c27787d5a2c143e67d943f96725ab7
Michael Williams <dinosaur@riseup.net>
2015-06-08 22:56:06 +1200

    fit arecord dat values between -1 and 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var mic = require('microphone')
 var through = require('through2')
 
 var byteRate = 2
-var numChannels = 2
+var numChannels = 1
 
 mic.startCapture({
   alsa_device: 'plughw:0,0',
@@ -15,19 +15,33 @@ mic.audioStream
 .pipe(through.obj(function (buf, enc, cb) {
   var numSamples = buf.length / (byteRate * numChannels)
 
-  var chunk = [[], []]
+  var chunk = [[]]
   var sampleIndex, channelIndex, offset
   for (sampleIndex = 0; sampleIndex < numSamples; sampleIndex++) {
     for (channelIndex = 0; channelIndex < numChannels; channelIndex++) {
       offset = sampleIndex + channelIndex
-      chunk[channelIndex][sampleIndex] = buf.readUInt16LE(offset)
+      chunk[channelIndex][sampleIndex] = buf.readUIntLE(offset, byteRate)
     }
   }
   this.push(chunk)
   cb()
 }))
 .pipe(through.obj(function (chunk, enc, cb) {
+  cb(null, chunk[0])
+}))
+.pipe(through.obj(fitWithinMag1))
+.pipe(through.obj(function (chunk, enc, cb) {
   this.push(JSON.stringify(chunk))
   cb()
 }))
 .pipe(process.stdout)
+
+var rangeFit = require('range-fit')
+
+function fitWithinMag1 (chunk, enc, cb) {
+  var minUInt = 0
+  var maxUInt = Math.pow(2, byteRate * 8) - 1
+  cb(null, chunk.map(function (val) {
+    return rangeFit(val, minUInt, maxUInt, -1.0, 1.0)
+  }))
+}
