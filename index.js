872bbb1c5356df0e134bc3dcdc0e43e761c0a6f5
Michael Williams <dinosaur@riseup.net>
2015-06-08 22:59:42 +1200

    analyse frequencies

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ mic.audioStream
   cb(null, chunk[0])
 }))
 .pipe(through.obj(fitWithinMag1))
+.pipe(through.obj(getFrequencies))
 .pipe(through.obj(function (chunk, enc, cb) {
   this.push(JSON.stringify(chunk))
   cb()
@@ -44,4 +45,10 @@ function fitWithinMag1 (chunk, enc, cb) {
   cb(null, chunk.map(function (val) {
     return rangeFit(val, minUInt, maxUInt, -1.0, 1.0)
   }))
+}
+
+var frequencyWorker = require('frequency-viewer').worker
+
+function getFrequencies (chunk, enc, cb) {
+  cb(null, frequencyWorker(chunk))
 }
