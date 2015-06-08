48130aea236e2e227b14cbb48e0b4c92ba4b45fc
Michael Williams <dinosaur@riseup.net>
2015-06-08 23:10:39 +1200

    loop data in circular buffer

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var mic = require('microphone')
 var through = require('through2')
+var rangeFit = require('range-fit')
+var Cbuffer = require('CBuffer')
+var frequencyWorker = require('frequency-viewer').worker
 
 var byteRate = 2
 var numChannels = 1
@@ -29,26 +32,35 @@ mic.audioStream
 .pipe(through.obj(function (chunk, enc, cb) {
   cb(null, chunk[0])
 }))
-.pipe(through.obj(fitWithinMag1))
-.pipe(through.obj(getFrequencies))
+.pipe(through.obj(fitWithinMag1()))
+.pipe(through.obj(circularize(4000)))
+.pipe(through.obj(getFrequencies()))
 .pipe(through.obj(function (chunk, enc, cb) {
   this.push(JSON.stringify(chunk))
   cb()
 }))
 .pipe(process.stdout)
 
-var rangeFit = require('range-fit')
-
-function fitWithinMag1 (chunk, enc, cb) {
-  var minUInt = 0
-  var maxUInt = Math.pow(2, byteRate * 8) - 1
-  cb(null, chunk.map(function (val) {
-    return rangeFit(val, minUInt, maxUInt, -1.0, 1.0)
-  }))
+function fitWithinMag1 () {
+  return function (chunk, enc, cb) {
+    var minUInt = 0
+    var maxUInt = Math.pow(2, byteRate * 8) - 1
+    cb(null, chunk.map(function (val) {
+      return rangeFit(val, minUInt, maxUInt, -1.0, 1.0)
+    }))
+  }
 }
 
-var frequencyWorker = require('frequency-viewer').worker
+function circularize (length) {
+  var cbuf = Cbuffer(length)
+  return function (chunk, enc, cb) {
+    cbuf.push.apply(cbuf, chunk)
+    cb(null, cbuf.toArray())
+  }
+}
 
-function getFrequencies (chunk, enc, cb) {
-  cb(null, frequencyWorker(chunk))
+function getFrequencies () {
+  return function (chunk, enc, cb) {
+    cb(null, frequencyWorker(chunk))
+  }
 }
