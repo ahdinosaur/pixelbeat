6ef8dc229632e298c6f0d0dfef2a8cf8104e8bc2
Michael Williams <dinosaur@riseup.net>
2015-07-04 20:07:40 +1200

    more legit

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var pixelsToTerminal = require('2dpixels-terminal')
 var Ndarray = require('ndarray')
 var zeros = require('zeros')
 var rangeFit = require('range-fit')
+var convert = require('ndpixels-convert')
+var assign = require('lodash.assign')
 
 play({
   //shape: [16, 16],
@@ -18,7 +20,7 @@ play({
   buffer: 1024,
   fps: 60,
   inc: 1,
-  value: 0.3
+  value: 30,
 })
 
 function play (opts) {
@@ -53,14 +55,17 @@ function play (opts) {
   }))
   .pipe(plotRms(opts))
   .pipe(overlayRainbow(opts))
-  .pipe(pixelsToTerminal(opts))
+  .pipe(convertStream('hsv', 'rgb'))
+  .pipe(pixelsToTerminal(assign({}, opts, {
+    shape: opts.shape.concat([3])
+  })))
   //.pipe(pixelsToOpc())
   //.pipe(socket)
 }
 
 function plotRms (opts) {
   return through.obj(function (rms, enc, cb) {
-    var frame = zeros(opts.shape)
+    var frame = zeros(opts.shape, 'uint8')
     var maxPoint= Math.max.apply(null, rms.data)
     var minPoint = Math.min.apply(null, rms.data)
     for (var x = 0; x < rms.shape[0]; x++) {
@@ -76,7 +81,7 @@ function plotRms (opts) {
 
 function overlayRainbow (opts) {
   var rainbow = rainbowPixels({
-    shape: opts.shape,
+    shape: opts.shape.concat([3]),
     inc: opts.inc,
     value: opts.value
   })
@@ -85,7 +90,8 @@ function overlayRainbow (opts) {
     for (var x = 0; x < frame.shape[0]; x++) {
       for (var y = 0; y < frame.shape[1]; y++) {
         if (!alpha.get(x, y)) {
-          frame.set(x, y, null)
+          // set value to 0
+          frame.set(x, y, 2, 0)
         }
       }
     }
@@ -96,4 +102,11 @@ function overlayRainbow (opts) {
 function getDecibels(value) {
   if (value == null) return 0
   return Math.round(Math.round(20 * (0.43429 * Math.log(value)) * 100) / 100 * 100) / 100
+}
+
+function convertStream (from, to) {
+  var converter = convert(from, to)
+  return through.obj(function (pixels, enc, cb) {
+    cb(null, converter(pixels))
+  })
 }
